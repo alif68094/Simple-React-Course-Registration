c0c4e2ca7c2160b2c077974546285c4151aedab5
Md. Alif <110423275+alif68094@users.noreply.github.com>
2023-09-14 21:13:00 +0600

    Basic projects structure

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-center text-3xl font-semibold'>Course Registration</h1>
+    </>
+  )
+}
+
+export default App
